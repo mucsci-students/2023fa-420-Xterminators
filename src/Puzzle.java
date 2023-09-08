050e893638565163ce7ec97b50411c2b1ef7b3c7
Jonathan Hart <jonathan.e.hart@outlook.com>
2023-09-07 20:27:24 -0400

    Added getters in Puzzle class
    Added getter for data members in the Puzzle class that will most likely be used outside of the class.

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Puzzle {
     private String seedWord;
@@ -8,4 +11,22 @@ public class Puzzle {
     private int totalPoints;
     private int earnedPoints;
     private int[] rankPoints;
+
+    public char[] getLetters() {
+        // returns a copy so that letters can not be modified externaly
+        return Arrays.copyOf(this.letters, this.letters.length);
+    }
+
+    public List<String> getFoundWords() {
+        // returns a read-only view of foundWords
+        return Collections.unmodifiableList(this.foundWords);
+    }
+
+    public int getTotalPoints() {
+        return this.totalPoints;
+    }
+
+    public int getEarnedPoints() {
+        return this.earnedPoints;
+    }
 }
