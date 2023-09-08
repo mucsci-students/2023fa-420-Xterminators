f640aa2681f6c230019da7ce71c0a67eb457421a
Jonathan Hart <jonathan.e.hart@outlook.com>
2023-09-07 20:07:15 -0400

    Created Puzzle Class
    Created the Puzzle class and added all the currently needed data members.

---
+++ src/Puzzle.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Puzzle {
+    private String seedWord;
+    private char[] letters;
+    private String[] validWords;
+    private ArrayList<String> foundWords;
+    private int totalPoints;
+    private int earnedPoints;
+    private int[] rankPoints;
+}
