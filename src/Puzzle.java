2fbdab2c240baeed45d450b02936951f7a0af506
Jonathan Hart <jonathan.e.hart@outlook.com>
2023-09-08 10:23:11 -0400

    Added Signatures for Needed Functions
    Added method signatures for guess, wordValue, and isPangram, which will all be needed to validate a word and update the puzzle state.

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -29,4 +29,39 @@ public class Puzzle {
     public int getEarnedPoints() {
         return this.earnedPoints;
     }
+    
+    /**
+     * Processes a guess of word for the puzzle. If the word is a valid guess,
+     * then the word will be added to foundWords, the point value of the word
+     * will be added to earnedPoints. The value of the word will be returned.
+     * 
+     * @param word The word the user guessed
+     * @return -1 if the word was already found,
+     *          0 if the word is not in validWords,
+     *          the number of points earned if the word is a valid guess
+     */
+    public int guess(String word) {
+        // TODO Implement checking logic
+    }
+
+    /**
+     * Calculates the point value of a word.
+     * 
+     * @param word The word to calculate the value of
+     * @return The point value of the word if it is a valid guess,
+     *         0 if the guess is invalid
+     */
+    private int wordValue(String word) {
+        // TODO Implement logic and fomulas
+    }
+
+    /**
+     * Checks if the word is a pangram (contains all seven puzzle letters)
+     * 
+     * @param word The word to check for pangram
+     * @return if the word is a pangram
+     */
+    private boolean isPangram(String word) {
+        // TODO Implement pangram check
+    }
 }
