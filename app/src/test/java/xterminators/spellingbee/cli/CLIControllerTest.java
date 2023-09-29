d0bc84aeecac5d493e3398f51bccf5906db689af
Jonathan Hart <jonathan.e.hart@outlook.com>
2023-09-28 20:30:07 -0400

    Added Input Helper Functions to Controller Test
    Added queueCommand and loadCommands to CLIControllerTest for ease of
    test writting.

--- app/src/test/java/xterminators/spellingbee/cli/CLIControllerTest.java
+++ app/src/test/java/xterminators/spellingbee/cli/CLIControllerTest.java
@@ -2,14 +2,19 @@ package xterminators.spellingbee.cli;
 
 import static org.mockito.Mockito.mock;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 public class CLIControllerTest {
     CLIView view;
     CLIController controller;
+
+    ArrayList<String> queuedCommands;
 
     @BeforeEach
     public void setup() {
@@ -23,6 +28,36 @@ public class CLIControllerTest {
 
         view = mock(CLIView.class);
 
+        queuedCommands = new ArrayList<>();
+
         controller = new CLIController(view, dictionaryFile, rootsDictionaryFile);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        queuedCommands = null;
+        view = null;
+        controller = null;
+    }
+
+    /**
+     * Adds a command string to be queued and executed by the controller. Has no
+     * effect if loadCommands is never called.
+     * 
+     * @param command The command to be added to the buffer
+     */
+    void queueCommand(String command) {
+        queuedCommands.add(command);
+    }
+
+    /**
+     * Takes all queued commands, places them in a input stream, and sets
+     * standard in to be that input stream.
+     */
+    void loadCommands() {
+        StringBuilder commands = new StringBuilder();
+        queuedCommands.stream().forEach(s -> commands.append(s).append('\n'));
+        ByteArrayInputStream testIn = new ByteArrayInputStream(commands.toString().getBytes());
+        System.setIn(testIn);
     }
 }
