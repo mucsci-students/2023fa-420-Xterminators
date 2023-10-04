c4bdfffe3cdd1896abf2e34fcc2421f8ebab8c87
Jonathan Hart <jonathan.e.hart@outlook.com>
2023-10-04 15:31:20 -0400

    Created CLIViewTest
    Created CLIViewTest and implemented tests for showFoundWords.

---
+++ app/src/test/java/xterminators/spellingbee/cli/CLIViewTest.java
@@ -0,0 +1,66 @@
+package xterminators.spellingbee.cli;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CLIViewTest {
+    private CLIView view;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setup() {
+        view = new CLIView();
+        System.setOut(new PrintStream(originalOut));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(originalOut);
+        view = null;
+    }
+
+    @Test
+    public void testShowFoundWords_Empty() {
+        List<String> foundWords = List.of();
+
+        view.showFoundWords(foundWords);
+
+        assertEquals(
+            "You have not found any words yet.\n",
+            outContent.toString()
+        );
+    }
+
+    @Test
+    public void testShowFoundWords_SingleWord() {
+        List<String> foundWords = List.of("offhanded");
+
+        view.showFoundWords(foundWords);
+
+        assertEquals(
+            "You have found 1 word:\noffhanded\n",
+            outContent.toString()
+        );
+    }
+
+    @Test
+    public void testShowFoundWords_MutipleWords() {
+        List<String> foundWords = List.of("offhanded", "offhand", "hand");
+
+        view.showFoundWords(foundWords);
+
+        assertEquals(
+            "You have found 3 words:\noffhanded\noffhand\nhand\n",
+            outContent.toString()
+        );
+    }
+}
