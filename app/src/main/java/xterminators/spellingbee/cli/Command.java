8bd93f2f68d3d141aa5730fd54a67fb95d57ff69
Jonathan Hart <56805467+JonathanMathBoi@users.noreply.github.com>
2023-10-07 19:20:59 +0000

    Added Short and Long Help Strings to Command

--- app/src/main/java/xterminators/spellingbee/cli/Command.java
+++ app/src/main/java/xterminators/spellingbee/cli/Command.java
@@ -8,25 +8,87 @@ import java.util.Optional;
  */
 public enum Command {
     /** The command to exit the CLI. */
-    EXIT("exit", "", ""),
+    EXIT(
+        "exit",
+        "Exits the game.",
+        """
+        Exits the game. This command will not save the puzzle before exiting. To
+        save see the "save" command."""
+    ),
     /** The command to display all found words. */
-    FOUND_WORDS("found", "", ""),
+    FOUND_WORDS(
+        "found",
+        "Shows the currently found words of the puzzle.",
+        "Shows the currently found words of the puzzle."
+    ),
     /** The command to guess a word. */
-    GUESS("guess", "", ""),
+    GUESS(
+        "guess",
+        "Guesses words for the puzzle.",
+        """
+        Guesses words for the puzzle. Requires at least one word to guess. Can
+        be used with as many words as wanted."""
+    ),
     /** The command to display command help. */
-    HELP("help", "", ""),
+    HELP(
+        "help",
+        "Shows help for commands.",
+        """
+        Shows help for commands. Add a command name after it for more detailed
+        help with that command."""
+    ),
     /** The command to load a puzzle from a file. */
-    LOAD("load", "", ""),
+    LOAD(
+        "load",
+        "Loads a puzzle from a file.",
+        """
+        Loads a puzzle from a file. Requires one argument containing the path to
+        to the file to load from."""
+    ),
     /** The command to create a new puzle. */
-    NEW("new", "", ""),
+    NEW(
+        "new",
+        "Creates a new puzzle.",
+        """
+        Creates a new puzzle at random or from a given word and letter. If a
+        a random word is wanted, do not add any parameters. If a puzle is wanted
+        from a word, the first argument should be a pangram, and the second
+        argument should be the required letter for the puzzle."""
+    ),
     /** The command to display ranks and current rank. */
-    RANK("rank", "", ""),
+    RANK(
+        "rank",
+        "Shows the rank for the puzzle.",
+        """
+        Shows all of the ranks for the puzzle, along with the necessary points
+        to achieve each rank. The current rank of the puzzle is given with a
+        star."""
+    ),
     /** The command to save the puzzle. */
-    SAVE("save", "", ""),
+    SAVE(
+        "save",
+        "Saves the puzzle to a file.",
+        """
+        Saves the puzzle to a file. Requires one argument that is the path of
+        where to save the puzzle."""
+    ),
     /** The command to display the puzzle. */
-    SHOW("show", "", ""),
+    SHOW(
+        "show",
+        "Displays the puzzle.",
+        """
+        Displays the puzzle. Shows the current puzzle with the required letter
+        in the center of the hexagon. Also shows the current rank and points of
+        the puzzle. To reorder the letter in the hexagon, see the "shuffle"
+        command."""
+    ),
     /** The command to shuffle the puzzle display. */
-    SHUFFLE("shuffle", "", "");
+    SHUFFLE(
+        "shuffle",
+        "Shuffles the letters in the puzzle.",
+        """
+        Shuffles the order of the non-required letters in the display of the puzzle."""
+    );
 
     /** The keyword for the command. */
     private String command;
