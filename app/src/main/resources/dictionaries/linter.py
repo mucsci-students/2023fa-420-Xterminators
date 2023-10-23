9f522e2b78288270a5075b66edf9680dcaa44391
Jonathan Hart <jonathan.e.hart@outlook.com>
2023-10-23 19:03:19 -0400

    Switched Dictionary to More Complete One
    Switched the dictionary to one that includes plurals. Updated
    `linter.py` to convert everything to lowercase, and created a better
    optimized valid dictionary.

--- app/src/main/resources/dictionaries/linter.py
+++ app/src/main/resources/dictionaries/linter.py
@@ -1,15 +1,16 @@
-x = open ('src\\dictionary.txt', 'r')
+x = open ('dictionary.txt', 'r')
 
 all_words = x.read()
 arr = all_words.split() 
 allowed = []
 
 for i in arr:
+    i = i.lower()
     chars = []
     for c in i:
         if (c not in chars):
             chars.append(c)
-    #if (len(chars) <= 7): # for getting all valid words
+    # if (len(i) >= 4): # for getting all valid words
     if (len(chars) == 7): # for getting only valid starting words
         allowed.append(i)
 
