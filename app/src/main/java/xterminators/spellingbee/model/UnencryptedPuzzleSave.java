9b1463f00e93f6af679c88364a6ebe10f57ed125
Jonathan Hart <jonathan.e.hart@outlook.com>
2023-11-02 19:37:09 -0400

    Added `UnencryptedPuzzleSave`  Class
    Added the `UnencryptedPuzzleSave` class for sotring a puzzle with the
    valid word list unencrypted. This essentially replaces PuzzleData.

---
+++ app/src/main/java/xterminators/spellingbee/model/UnencryptedPuzzleSave.java
@@ -0,0 +1,37 @@
+package xterminators.spellingbee.model;
+
+import java.util.List;
+
+/**
+ * A PuzzleSave class for storing a puzzle with an unencrypted word list.
+ */
+public class UnencryptedPuzzleSave extends PuzzleSave {
+    private List<String> validWords;
+
+    /**
+     * Constructs a new UnencryptedPuzzleSave.
+     * 
+     * @param baseWord an array containing all the letters of the puzzle
+     * @param requiredLetter the required letter of the puzzle
+     * @param foundWords the player's found words
+     * @param playerPoints the player's earned points
+     * @param validWords the list of valid words
+     * @param maxPoints the maximum points of the puzzle
+     */
+    public UnencryptedPuzzleSave(
+        char[] baseWord,
+        char requiredLetter,
+        List<String> foundWords,
+        int playerPoints,
+        List<String> validWords,
+        int maxPoints
+    ) {
+        super(baseWord, requiredLetter, foundWords, playerPoints, maxPoints);
+        this.validWords = validWords;
+    }
+
+    @Override
+    public List<String> validWords() {
+        return validWords;
+    }
+}
