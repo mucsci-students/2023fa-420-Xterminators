a6eb5eac5c60a3936c8c1d2dbc2c33576fa187e6
Jonathan Hart <jonathan.e.hart@outlook.com>
2023-11-14 14:26:16 -0500

    Added Tests to EncryptedPuzzleSave

--- app/src/test/java/xterminators/spellingbee/model/EncryptedPuzzleSaveTest.java
+++ app/src/test/java/xterminators/spellingbee/model/EncryptedPuzzleSaveTest.java
@@ -1,16 +1,21 @@
 package xterminators.spellingbee.model;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class EncryptedPuzzleSaveTest {
-    @Test
-    public void testEncryptDecrypt() {
-        PuzzleSave puzzleSave = assertDoesNotThrow(
+    private PuzzleSave puzzleSave;
+
+    @BeforeEach
+    public void setUp() {
+        puzzleSave = assertDoesNotThrow(
             () -> new EncryptedPuzzleSave(
                 new char[] {'g', 'u', 'a', 'r', 'd', 'i', 'n'},
                 'a',
@@ -21,7 +26,15 @@ public class EncryptedPuzzleSaveTest {
             ),
             "EncryptedPuzzleSave constructor should not throw an exception"
         );
+    }
 
+    @AfterEach
+    public void tearDown() {
+        puzzleSave = null;
+    }
+
+    @Test
+    public void testEncryptDecrypt() {
         List<String> validWords = assertDoesNotThrow(
             () -> puzzleSave.validWords(),
             "EncryptedPuzzleSave.validWords() should not throw an exception"
@@ -32,6 +45,65 @@ public class EncryptedPuzzleSaveTest {
             validWords,
             "EncryptedPuzzleSave.validWords() should return the correct list" 
             + "of valid words"
+        );
+    }
+
+    @Test
+    public void testBaseWord() {
+        char[] baseWord = puzzleSave.baseWord();
+
+        assertArrayEquals(
+            new char[] {'g', 'u', 'a', 'r', 'd', 'i', 'n'},
+            baseWord,
+            "EncryptedPuzzleSave.baseWord() should return the correct base word"
+        );
+    }
+
+    @Test
+    public void testRequiredLetter() {
+        char requiredLetter = puzzleSave.requiredLetter();
+
+        assertEquals(
+            'a',
+            requiredLetter,
+            "EncryptedPuzzleSave.requiredLetter() should return the correct"
+            + "required letter"
+        );
+    }
+
+    @Test
+    public void testFoundWords() {
+        List<String> foundWords = puzzleSave.foundWords();
+
+        assertEquals(
+            List.of("guard","guardian"),
+            foundWords,
+            "EncryptedPuzzleSave.foundWords() should return the correct list"
+            + "of found words"
+        );
+    }
+
+    @Test
+    public void testPlayerPoints() {
+        int playerPoints = puzzleSave.playerPoints();
+
+        assertEquals(
+            20,
+            playerPoints,
+            "EncryptedPuzzleSave.playerPoints() should return the correct"
+            + "player points"
+        );
+    }
+
+    @Test
+    public void testMaxPoints() {
+        int maxPoints = puzzleSave.maxPoints();
+
+        assertEquals(
+            29,
+            maxPoints,
+            "EncryptedPuzzleSave.maxPoints() should return the correct max"
+            + "points"
         );
     }
 }
